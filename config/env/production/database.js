2a254e984a506cdd4792bfaccaf902b4dd0193b1
Santiago Nicolas <nicolas.cba.129@gmail.com>
2023-02-03 18:24:21 -0300

    strapi in railway connected

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -2,12 +2,12 @@ module.exports = ({ env }) => ({
     connection: {
         client: 'postgres',
         connection: {
-            host: env('PGHOST', '127.0.0.1'),
-            port: env.int('PGPORT', 5931),
+            host: env('PGHOST', '0.0.0.0'),
+            port: env.int('PGPORT', 7931),
             database: env('PGDATABASE', 'railway'),
             user: env('PGUSER', 'postgres'),
             password: env('PGPASSWORD', 'password'),
-            ssl: env.bool(true),
+            ssl: env.bool(false),
         },
     },
 });
