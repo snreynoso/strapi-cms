8758f426a357cec05c7d9cd961829755e14e4129
Santiago Nicolas <nicolas.cba.129@gmail.com>
2023-02-06 14:35:30 -0300

    Update database.js

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
         client: 'postgres',
         connection: {
             host: env('PGHOST', '0.0.0.0'),
-            port: env.int('PGPORT', 5931),
+            port: env.int('PGPORT', 7931),
             database: env('PGDATABASE', 'railway'),
             user: env('PGUSER', 'postgres'),
             password: env('PGPASSWORD', 'password'),
