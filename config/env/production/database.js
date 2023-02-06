ad84caa5c7f2e35be5c32ee51f6b76c8d3302467
Santiago Nicolas <nicolas.cba.129@gmail.com>
2023-02-06 17:36:25 -0300

    env.production

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
     connection: {
         client: 'postgres',
         connection: {
-            host: env('PGHOST', '0.0.0.0'),
+            host: env('PGHOST', '127.0.0.1'),
             port: env.int('PGPORT', 5931),
             database: env('PGDATABASE', 'railway'),
             user: env('PGUSER', 'postgres'),
